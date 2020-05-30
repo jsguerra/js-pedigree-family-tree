f91a0bc71055cc8fb82c9c93508ad05a5fc5e874
Jose Guerra <jose@gcostudios.com>
2020-05-30 15:03:44 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ['./src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'app'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './app'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // After all CSS loaders we use plugin to do this work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader"
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
