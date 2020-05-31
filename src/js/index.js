e68494f2e0dcd7cde0feef4d90996a63312949ad
Jose Guerra <jose@gcostudios.com>
2020-05-31 11:20:22 +0100

    added concept description

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
 import '../sass/style.scss';
 
 console.log('Webpack Boilerplate');
+
+// Simple data structure
+// 1. add home person
+// 2. add home person details (birth, marriage, death, etc)
+// 3. add parent with repeated details above
+// 4. destinguish between father and mother
+
+// Table structure
+// 1. if only 1 person
+//   a. 2 rows are initially added
+//   b. 1 cell per row is added
+// 2. if a parent is added
+//   a. cell added to existing row
+//   b. rowspan added to first cell
+//   c. new row added with new cell
+// 3. distinguish between father and mother
+//   a. if father add cell to first row
+//     and add second row below first row
+//   b. if mother add cell to second row
+//     and add second row below second row
