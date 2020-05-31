9f859a7c4c704279e52cf6f341b2681f488d342f
Jose Guerra <jose@gcostudios.com>
2020-05-31 12:25:02 +0100

    created person object

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,32 @@ console.log('Webpack Boilerplate');
 // 3. add parent with repeated details above
 // 4. destinguish between father and mother
 
+// Person setup
+// ===============================================
+const person = {
+  name: 'Home Person',
+  bio: {
+    birthYear: 1940,
+    deathYear: 2018,
+  },
+  parents: {
+    father: {
+      name: 'father',
+      bio: {
+        birthYear: 1900,
+        deathYear: 1960
+      }
+    },
+    mother: {
+      name: 'mother',
+      bio: {
+        birthYear: 1900,
+        deathYear: 1960
+      }
+    }
+  }
+}
+
 // Table structure
 // 1. if only 1 person
 //   a. 2 rows are initially added
