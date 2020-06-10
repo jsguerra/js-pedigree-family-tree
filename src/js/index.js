fcdfa247c26fa8b85d6ac68fe61421b1a00832e8
Jose Guerra <jose.guerra@finalsite.com>
2020-06-10 22:44:47 +0100

    updated structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,42 @@
 import '../sass/style.scss';
 import family from './person';
 
+// Count the number of persons in the family object
+// const keyCount = Object.keys(family.family).length;
+// console.log(keyCount);
+
+// for (let i = 0; i < familyGroup.length; i++) {
+//   console.log(Math.pow(2, (i + 1)));
+// }
+
+// Setup variables
+// ===============================================
+const pedigreeContainer = document.querySelectorAll('.pedigree-view');
+const familyGroup = family.family;
+
+console.log(`Array length = ${familyGroup.length}`);
+
+// Create cells
+// ================================================
+const createCell = () => {
+  // create a template for the cell
+  const cell = `<td rowspan="1" class="m generation-">test</td>`;
+
+  return cell;
+}
+
+// Create rows
+// ================================================
+const createRow = () => {
+  const rows = [];
+
+  for (let i = 0; i < familyGroup.length; i++) {
+    let row = `<tr></tr>`;
+
+    rows.push(row);
+  }
+}
+
 // Table structure
 // 1. if only 1 person
 //   a. 2 rows are initially added
@@ -15,47 +51,14 @@ import family from './person';
 //   b. if mother add cell to second row
 //     and add second row below second row
 
-// Count the number of persons in the family object
-// const keyCount = Object.keys(family.family).length;
-// console.log(keyCount);
-
 // Rules for the loop:
 // if familyArray = 1 then add 2 rows 2 cells
 // if familyArray is at least = 2 then add 4 rows 8 cells
 // if familyArray is at least = 4 then add 8 rows 24 cells
 
-// for (let i = 0; i < familyGroup.length; i++) {
-//   console.log(Math.pow(2, (i + 1)));
-// }
-
-// Setup variables
-// ===============================================
-const pedigreeContainer = document.querySelectorAll('.pedigree-view');
-const familyGroup = family.family.map(el => console.log(el));
-console.log(`Array length = ${familyGroup.length}`);
-
-// Create cells
-// ================================================
-const createCell = (content, rowspan) => {
-  // create a template for the cell
-  const cell = `<td rowspan="${rowspan}" class="">${content}</td>`;
-
-  return cell;
-}
-
-// Create rows
-// ================================================
-const createRow = (content, rowspan) => {
-  const row = `<tr>
-    ${createCell(content, rowspan)}
-  </tr>`;
-
-  return row;
-}
-
 // Create the table pedigree
 // ================================================
-// Test for class
+// Test to see if Pedigree class exists
 if (pedigreeContainer) {
 
   // Look for all pedigrees
@@ -63,7 +66,7 @@ if (pedigreeContainer) {
     const table = `
     <table border="1px">
       <tbody>
-        ${createRow('text', 1)}
+        ${createRow()}
       </tbody>
     </table>
     `;
@@ -71,3 +74,41 @@ if (pedigreeContainer) {
     elContainer.insertAdjacentHTML('afterbegin', table);
   }
 }
+
+function tableCreate() {
+  //body reference 
+  var body = document.getElementsByTagName('body')[0];
+
+  // create elements <table> and a <tbody>
+  var tbl     = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  // cells creation
+  for (var j = 0; j <= 2; j++) {
+      // table row creation
+      var row = document.createElement("tr");
+
+      for (var i = 0; i < 2; i++) {
+          // create element <td> and text node 
+          //Make text node the contents of <td> element
+          // put <td> at end of the table row
+          var cell = document.createElement("td");    
+          var cellText = document.createTextNode("cell is row "+j+", column "+i); 
+
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+      }
+
+      //row added to end of table body
+      tblBody.appendChild(row);
+  }
+
+  // append the <tbody> inside the <table>
+  tbl.appendChild(tblBody);
+  // put <table> in the <body>
+  body.appendChild(tbl);
+  // tbl border attribute to 
+  tbl.setAttribute("border", "2");
+}
+
+tableCreate();
