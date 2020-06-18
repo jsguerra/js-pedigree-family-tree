a91a52d71841a99a8af57d8e1d63e43d263a4c0e
Jose Guerra <jose.guerra@finalsite.com>
2020-06-18 23:56:04 +0100

    updated syntax

--- src/js/person.js
+++ src/js/person.js
@@ -12,40 +12,56 @@ export const family = [
     "generation": 1,
     "name": "Home Person",
     "gender": "m",
-    "birthYear": 1940,
-    "deathYear": 2018,
-    "father": 2,
-    "mother": 3
+    "bio": {
+      "birthYear": 1940,
+      "deathYear": 2018
+    },
+    "parents": {
+      "father": 2,
+      "mother": 3
+    }
   },
   {
     "id": 2,
     "generation": 2,
     "name": "Father Person",
     "gender": "m",
-    "birthYear": 1900,
-    "deathYear": 1980,
-    "father": "",
-    "mother": ""
+    "bio": {
+      "birthYear": 1900,
+      "deathYear": 1980
+    },
+    "parents": {
+      "father": 4,
+      "mother": ""
+    }
   },
   {
     "id": 3,
     "generation": 2,
     "name": "Mother Person",
     "gender": "f",
-    "birthYear": 1900,
-    "deathYear": 1980,
-    "father": "",
-    "mother": ""
+    "bio": {
+      "birthYear": 1900,
+      "deathYear": 1980
+    },
+    "parents": {
+      "father": "",
+      "mother": ""
+    }
   },
   {
     "id": 4,
     "generation": 3,
     "name": "Father Person",
     "gender": "m",
-    "birthYear": 1880,
-    "deathYear": 1920,
-    "father": "",
-    "mother": ""
+    "bio": {
+      "birthYear": 1880,
+      "deathYear": 1920
+    },
+    "parents": {
+      "father": "",
+      "mother": ""
+    }
   }
 ]
 
