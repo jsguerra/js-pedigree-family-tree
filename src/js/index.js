529aecbe78219df85304cf1221218ec58ec55781
Jose Guerra <jose.guerra@finalsite.com>
2020-06-19 21:45:49 +0100

    updated table

--- src/js/index.js
+++ src/js/index.js
@@ -85,38 +85,40 @@ if (pedigreeContainer) {
 
 function tableCreate() {
   //body reference 
-  var body = document.getElementsByTagName('body')[0];
+  // const body = document.getElementsByTagName('body')[0];
+  const container = document.querySelector('.container');
 
   // create elements <table> and a <tbody>
-  var tbl     = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+  const tbl     = document.createElement("table");
+  const tblBody = document.createElement("tbody");
 
   // cells creation
-  for (var j = 0; j <= 2; j++) {
-      // table row creation
-      var row = document.createElement("tr");
+  for (let j = 0; j <= 2; j++) {
+    // table row creation
+    const row = document.createElement("tr");
 
-      for (var i = 0; i < 2; i++) {
-          // create element <td> and text node 
-          //Make text node the contents of <td> element
-          // put <td> at end of the table row
-          var cell = document.createElement("td");    
-          var cellText = document.createTextNode("cell is row "+j+", column "+i); 
+    for (let i = 0; i < 2; i++) {
+      // create element <td> and text node 
+      //Make text node the contents of <td> element
+      // put <td> at end of the table row
+      const cell = document.createElement("td");    
+      let cellText = document.createTextNode("cell is row "+j+", column "+i); 
 
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-      }
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
 
-      //row added to end of table body
-      tblBody.appendChild(row);
+    //row added to end of table body
+    tblBody.appendChild(row);
   }
 
   // append the <tbody> inside the <table>
   tbl.appendChild(tblBody);
   // put <table> in the <body>
-  body.appendChild(tbl);
+  container.appendChild(tbl);
   // tbl border attribute to 
-  tbl.setAttribute("border", "2");
+  tbl.setAttribute("border", "1");
+  tbl.classList.add("created-table");
 }
 
 tableCreate();
